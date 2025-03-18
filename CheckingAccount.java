4066a5bf48a51473c903fd1d7bab84cdcb19911d
SamAllen06 <samgallen06@gmail.com>
2025-03-18 21:56:37 +0000

    got checkingAccount working

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -33,14 +33,15 @@ public class CheckingAccount{
 			}//end if
 			else if (userChoice.equals("1")){
 				System.out.println("Showing balance");
-				//System.out.println(getBalanceString);
+				System.out.println(getBalanceString());
 			}//end else if
 			else if (userChoice.equals("2")){
 				System.out.println("Deposit money");
-				//makeDeposit();
+				makeDeposit();
 			}//end else if
 			else if (userChoice.equals("3")){
 				System.out.println("Withdraw money");
+				makeWithdrawal();
 			}//end else if
 			else{
 				System.out.println("I didn't understand that. Try again");
@@ -49,31 +50,55 @@ public class CheckingAccount{
 	}//end start definition
 
 	public double getBalance(){
-	
+		return this.balance;
 	}//end getBalance def
 
 	public String getBalanceString(){
-		
+		String strBalance = String.valueOf(this.balance);
+		String formattedStrBalance = String.format("$%s", strBalance);
+		return formattedStrBalance;
 	}//end getBalanceString def
 
 	public void setBalance(double balance){
-
+		this.balance = balance;
 	}//end setBalance def
 
 	public void checkBalance(){
-
+		System.out.println(getBalanceString());
 	}//end checkBalance def
 
 	private double getDouble(){
-
+		boolean keepGoing = true;
+		Scanner input = new Scanner(System.in);
+		String userInput = input.nextLine();
+		double userDouble = 0d;
+		while (keepGoing){
+			try{
+				userDouble = Double.parseDouble(userInput);
+				keepGoing = false;
+			}//end try
+			catch(NumberFormatException e){
+				System.out.println("Invalid input. Try again.");
+			}//end catch
+		}//end while loop
+		return userDouble;
 	}//end getDouble def
 
 	public void makeDeposit(){
-
+		System.out.print("How much would you like to deposit? ");
+		double depositDouble = getDouble();
+		this.balance = depositDouble + this.balance;
 	}//end makeDeposit def
 
 	public void makeWithdrawal(){
-
+		System.out.print("How much would you like to withdraw? ");
+		double withdrawalDouble = getDouble();
+		if (withdrawalDouble > this.balance){
+			System.out.println("You can't withdraw more money than what's in your account. ");
+		}//end if
+		else{
+			this.balance = this.balance - withdrawalDouble;
+		}//end else
 	}//end makeWithdrawal def
 
 }//end CheckingAccount def
