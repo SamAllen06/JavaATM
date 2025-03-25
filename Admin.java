e5fcc0e9d883fba1b8097445bd70db846fb23393
SamAllen06 <samgallen06@gmail.com>
2025-03-25 15:33:40 +0000

    added Admin class

---
+++ Admin.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Admin extends User{
+	public void Admin(){
+		this.setUserName("admin");
+		this.setPIN("0000");
+	}//end constructor
+	
+	public void start(){};
+
+	public String menu(){
+		System.out.print("Admin Menu: \n0) Exit \n1) Full customer report \n2) Add user \n3)Apply interest to savings accounts\nAction: ");
+		Scanner input = new Scanner(System.in);
+		String response = input.nextLine();
+		return response;
+	}// end menu def
+
+	public String getReport(){
+		String adminReport = getUserName() + ", " + getPIN();
+		return adminReport;
+	}//end getReport def
+}//end class def
